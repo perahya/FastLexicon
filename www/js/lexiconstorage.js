41f5c114753f65879123a9ab8855c656176ef155
perahya <perahya@hotmail.com>
2014-07-31 14:42:52 +0200

    new data model

--- www/js/lexiconstorage.js
+++ www/js/lexiconstorage.js
@@ -2,16 +2,9 @@
         this._my_lexicon_id = 'my_lexicon'; //nom de la base perso
         //this._all_hebrew_words_lexicon_id = 'all_hebrew_words_lexicon';
         this._all_hebrew_words_lexicon_id = 'all_hebrew';
-                
-        /*this.getLexicon = function(fctDone) {            
-            var ls = window.localStorage;
-            console.log('local storage: ' + ls);
-            var myLexicon = JSON.parse(ls.getItem(keyName));                     
-            fctDone(myLexicon);                      
-        }*/
-     
+                            
         this.getMyLexicon = function(fctDone) {            
-            this.getLexiconById('my_lexicon', fctDone);                    
+            this.getLexiconById(this._my_lexicon_id, fctDone);                    
         } 
         
         this.getLexiconById = function(lexiconId, fctDone) {            
@@ -24,24 +17,19 @@
                 var ls = window.localStorage;
                 var key = lexiconId;
             
-                var lexicon = JSON.parse(ls.getItem(key));
-                if (lexicon != null && typeof(lexicon) != 'undefined')
-                {
-                    lexicon._lexicon_id = lexiconId;
-                }
+                var lexicon = JSON.parse(ls.getItem(key));                
                 fctDone(lexicon);
             }
         }
         
         this.saveLexicon = function(lexicon, fctDone) {                                      
-            if (lexicon == null || typeof(lexicon) == 'undefined' ||
-                lexicon._lexicon_id == null || typeof(lexicon._lexicon_id) == 'undefined' || lexicon._lexicon_id.length == 0)
+            if (lexicon == null || typeof(lexicon) == 'undefined')
             {
                 console.out("lexicon not specified");
                 return;
             }
             var localData = JSON.stringify(lexicon);	
-            var keyName = lexicon._lexicon_id;
+            var keyName = this._my_lexicon_id;
             window.localStorage.setItem(keyName, localData);
                         
             fctDone();
